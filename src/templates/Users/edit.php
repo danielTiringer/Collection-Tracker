1f6d668a2059e6091bb5279bea019beabc80d8ea
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-05-28 20:54:55 +0200

    Revamp the profile page

--- src/templates/Users/edit.php
+++ src/templates/Users/edit.php
@@ -5,30 +5,32 @@
  */
 ?>
 <div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Form->postLink(
-                __('Delete'),
-                ['action' => 'delete', $user->id],
-                ['confirm' => __('Are you sure you want to delete # {0}?', $user->id), 'class' => 'side-nav-item']
-            ) ?>
-            <?= $this->Html->link(__('List Users'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
+    <div class="column-responsive column-100">
         <div class="users form content">
             <?= $this->Form->create($user) ?>
             <fieldset>
-                <legend><?= __('Edit User') ?></legend>
+                <legend><?= __('Edit Profile') ?></legend>
                 <?php
                     echo $this->Form->control('name');
                     echo $this->Form->control('email');
-                    echo $this->Form->control('password');
+                    echo $this->Form->control('password', [
+                        'value' => '',
+                    ]);
+                    echo $this->Form->control('password_confirm', [
+                        'type' => 'password',
+                    ]);
                 ?>
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
             <?= $this->Form->end() ?>
         </div>
+        <?= $this->Form->postLink(
+            __('Delete'),
+            ['action' => 'delete', $user->id],
+            [
+                'confirm' => __('Are you sure you want to delete your account?'),
+                'class' => 'button margin-20',
+            ]
+        ) ?>
     </div>
 </div>
