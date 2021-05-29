03c94ea4cadbb7c24a97886e78424008bc887d71
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-05-29 16:03:39 +0200

    Make migration to add image to collection

---
+++ src/config/Migrations/20210529125924_AddImageToCollections.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddImageToCollections extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('collections');
+        $table->addColumn('image', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+            'after' => 'goal',
+        ]);
+        $table->update();
+    }
+}
