1a4f8cd8fa58c7f578f4bde8038e0c74a0d35b8b
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-06-08 16:27:56 +0200

    Make view for adding a new element

---
+++ src/templates/Elements/add.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\Element $element
+ */
+?>
+<div class="row">
+    <div class="column-responsive column-100">
+        <div class="elements form content">
+            <?= $this->Form->create($element) ?>
+            <fieldset>
+                <legend><?= __('Add Element') ?></legend>
+                <?php
+                    echo $this->Form->control('name');
+                    echo $this->Form->control('description');
+                    echo $this->Form->hidden('collection_id', ['value' => 2]);
+                    echo $this->Form->control('source');
+                    /* echo $this->Form->control('metadata'); */
+                ?>
+            </fieldset>
+            <?= $this->Form->button(__('Submit')) ?>
+            <?= $this->Form->end() ?>
+        </div>
+    </div>
+</div>
