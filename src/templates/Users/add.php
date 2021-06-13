8eb87a441dcc8ae3a7323e8c6712490b47320266
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-06-13 21:18:06 +0200

    Use view component in register form

--- src/templates/Users/add.php
+++ src/templates/Users/add.php
@@ -3,37 +3,39 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\User $user
  */
-?>
-<div class="form-group d-flex justify-content-center">
-    <?= $this->Form->create($user) ?>
-    <fieldset>
-        <legend><?= __('Register') ?></legend>
-        <?php
-            echo $this->Form->control('name', [
-                'class' => 'form-control',
-            ]);
-            echo $this->Form->control('email', [
-                'class' => 'form-control',
-            ]);
-            echo $this->Form->control('password', [
-                'class' => 'form-control',
-            ]);
-            echo $this->Form->control('password_confirm', [
-                'class' => 'form-control',
-                'type' => 'password',
-            ]);
-        ?>
-    </fieldset>
 
-    <div class="mt-2 d-flex justify-content-between">
-        <?= $this->Form->button(__('Submit'), [
-            'class' => 'btn btn-outline-danger',
-        ]) ?>
-        <?= $this->Form->end() ?>
-        <?= $this->Html->link(
-            "Got an account already?",
-            'login',
-            ['class' => 'text-danger']
-        ) ?>
-    </div>
-</div>
+
+$this->extend('../Common/form');
+$this->assign('title', __('Register'));
+
+$this->start('form-creation');
+    echo $this->Form->create($user);
+$this->end();
+
+$this->start('form-fields');
+    echo $this->Form->control('name', [
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->control('email', [
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->control('password', [
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->control('password_confirm', [
+        'class' => 'form-control',
+        'type' => 'password',
+    ]);
+$this->end();
+
+$this->start('form-end');
+    echo $this->Form->button(__('Submit'), [
+        'class' => 'btn btn-outline-danger',
+    ]);
+    echo $this->Form->end();
+    echo $this->Html->link(
+        "Got an account already?",
+        'login',
+        ['class' => 'btn btn-outline-danger']
+    );
+$this->end();
