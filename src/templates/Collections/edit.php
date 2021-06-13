366d1477e23df0664ef2f3b3ca8a4048b605d5cf
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-06-13 20:28:07 +0200

    Update indentation

--- src/templates/Collections/edit.php
+++ src/templates/Collections/edit.php
@@ -5,38 +5,38 @@
  */
 ?>
 <div class="form-group d-flex justify-content-center">
-        <?= $this->Form->create($collection, ['type' => 'file']) ?>
-        <fieldset>
-            <legend><?= __('Edit Collection') ?></legend>
-            <?php
-                echo $this->Form->control('name', [
-                    'class' => 'form-control',
-                ]);
-                echo $this->Form->control('description', [
-                    'class' => 'form-control',
-                ]);
-                echo $this->Form->control('goal', [
-                    'class' => 'form-control',
-                ]);
-                echo $this->Html->image(
-                    '/img/collection-img/' . $collection->image,
-                    ['class' => 'height-200 mt-2']
-                );
-                echo $this->Form->control('image_file', [
-                    'type' => 'file',
-                    'class' => 'form-control',
-                ]);
-            ?>
-        </fieldset>
-        <div class="mt-2 d-flex justify-content-between">
-            <?= $this->Form->button(__('Submit'), [
-                'class' => 'btn btn-outline-danger',
-            ]) ?>
-            <?= $this->Form->end() ?>
-            <?= $this->Html->link(
-                __('Back'),
-                ['action' => 'index'],
-                ['class' => 'btn btn-outline-danger'])
-            ?>
-        </div>
+    <?= $this->Form->create($collection, ['type' => 'file']) ?>
+    <fieldset>
+        <legend><?= __('Edit Collection') ?></legend>
+        <?php
+            echo $this->Form->control('name', [
+                'class' => 'form-control',
+            ]);
+            echo $this->Form->control('description', [
+                'class' => 'form-control',
+            ]);
+            echo $this->Form->control('goal', [
+                'class' => 'form-control',
+            ]);
+            echo $this->Html->image(
+                '/img/collection-img/' . $collection->image,
+                ['class' => 'height-200 mt-2']
+            );
+            echo $this->Form->control('image_file', [
+                'type' => 'file',
+                'class' => 'form-control',
+            ]);
+        ?>
+    </fieldset>
+    <div class="mt-2 d-flex justify-content-between">
+        <?= $this->Form->button(__('Submit'), [
+            'class' => 'btn btn-outline-danger',
+        ]) ?>
+        <?= $this->Form->end() ?>
+        <?= $this->Html->link(
+            __('Back'),
+            ['action' => 'index'],
+            ['class' => 'btn btn-outline-danger'])
+        ?>
+    </div>
 </div>
