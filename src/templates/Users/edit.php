81d770129836a6093e7ae2219c732da877caff21
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-06-13 09:57:12 +0200

    Update the css on the profile form

--- src/templates/Users/edit.php
+++ src/templates/Users/edit.php
@@ -4,36 +4,40 @@
  * @var \App\Model\Entity\User $user
  */
 ?>
-<div class="row">
-    <div class="column-responsive column-100">
-        <div class="users form content">
-            <?= $this->Form->create($user) ?>
-            <fieldset>
-                <legend><?= __('Edit Profile') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('email');
-                    echo $this->Form->control('password', [
-                        'value' => '',
-                    ]);
-                    echo $this->Form->control('password_confirm', [
-                        'type' => 'password',
-                    ]);
-                ?>
-            </fieldset>
+<div class="form-group d-flex justify-content-center">
+    <?= $this->Form->create($user) ?>
+    <fieldset>
+        <legend><?= __('Edit Profile') ?></legend>
+        <?php
+            echo $this->Form->control('name', [
+                'class' => 'form-control',
+            ]);
+            echo $this->Form->control('email', [
+                'class' => 'form-control',
+            ]);
+            echo $this->Form->control('password', [
+                'value' => '',
+                'class' => 'form-control',
+            ]);
+            echo $this->Form->control('password_confirm', [
+                'type' => 'password',
+                'class' => 'form-control',
+            ]);
+        ?>
+    </fieldset>
 
-            <div class="flex-space-between">
-                <?= $this->Form->button(__('Submit')) ?>
-                <?= $this->Form->end() ?>
-                <?= $this->Form->postLink(
-                    __('Delete'),
-                    ['action' => 'delete', $user->id],
-                    [
-                        'confirm' => __('Are you sure you want to delete your account?'),
-                        'class' => 'button',
-                    ]
-                ) ?>
-            </div>
-        </div>
+    <div class="mt-2">
+        <?= $this->Form->button(__('Submit'), [
+            'class' => 'btn btn-outline-danger',
+        ]) ?>
+        <?= $this->Form->end() ?>
+        <?= $this->Form->postLink(
+            __('Delete'),
+            ['action' => 'delete', $user->id],
+            [
+                'confirm' => __('Are you sure you want to delete your account?'),
+                'class' => 'text-danger',
+            ]
+        ) ?>
     </div>
 </div>
