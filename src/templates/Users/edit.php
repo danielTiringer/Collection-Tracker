ac76c273eed07cf1c8fe7f74363f3f29ae87e975
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-06-13 21:18:20 +0200

    Use view component in profile form

--- src/templates/Users/edit.php
+++ src/templates/Users/edit.php
@@ -3,41 +3,42 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\User $user
  */
-?>
-<div class="form-group d-flex justify-content-center">
-    <?= $this->Form->create($user) ?>
-    <fieldset>
-        <legend><?= __('Edit Profile') ?></legend>
-        <?php
-            echo $this->Form->control('name', [
-                'class' => 'form-control',
-            ]);
-            echo $this->Form->control('email', [
-                'class' => 'form-control',
-            ]);
-            echo $this->Form->control('password', [
-                'value' => '',
-                'class' => 'form-control',
-            ]);
-            echo $this->Form->control('password_confirm', [
-                'type' => 'password',
-                'class' => 'form-control',
-            ]);
-        ?>
-    </fieldset>
 
-    <div class="mt-2 d-flex justify-content-between">
-        <?= $this->Form->button(__('Submit'), [
+$this->extend('../Common/form');
+$this->assign('title', __('Edit Profile'));
+
+$this->start('form-creation');
+    echo $this->Form->create($user);
+$this->end();
+
+$this->start('form-fields');
+    echo $this->Form->control('name', [
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->control('email', [
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->control('password', [
+        'value' => '',
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->control('password_confirm', [
+        'type' => 'password',
+        'class' => 'form-control',
+    ]);
+$this->end();
+
+$this->start('form-end');
+    echo $this->Form->button(__('Submit'), [
+        'class' => 'btn btn-outline-danger',
+    ]);
+    echo $this->Form->end();
+    echo $this->Form->postLink(
+        __('Delete'),
+        ['action' => 'delete', $user->id],
+        [
+            'confirm' => __('Are you sure you want to delete your account?'),
             'class' => 'btn btn-outline-danger',
-        ]) ?>
-        <?= $this->Form->end() ?>
-        <?= $this->Form->postLink(
-            __('Delete'),
-            ['action' => 'delete', $user->id],
-            [
-                'confirm' => __('Are you sure you want to delete your account?'),
-                'class' => 'btn btn-outline-danger',
-            ]
-        ) ?>
-    </div>
-</div>
+        ]
+    );
+$this->end();
