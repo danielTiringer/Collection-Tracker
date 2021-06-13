7d5b9af184af1253892045b2021a7fc8a8868d58
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-06-13 20:57:38 +0200

    Create template and refactor add element form

---
+++ src/templates/Common/form.php
@@ -0,0 +1,10 @@
+<div class="form-group d-flex justify-content-center">
+    <?= $this->fetch('form-creation') ?>
+    <fieldset>
+        <legend><?= $this->fetch('title') ?></legend>
+        <?= $this->fetch('form-fields') ?>
+    </fieldset>
+    <div class="mt-2 d-flex justify-content-between">
+        <?= $this->fetch('form-end') ?>
+    </div>
+</div>
--- src/templates/Elements/add.php
+++ src/templates/Elements/add.php
@@ -3,23 +3,36 @@
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Element $element
  */
-?>
-<div class="row">
-    <div class="column-responsive column-100">
-        <div class="elements form content">
-            <?= $this->Form->create($element) ?>
-            <fieldset>
-                <legend><?= __('Add Element') ?></legend>
-                <?php
-                    echo $this->Form->control('name');
-                    echo $this->Form->control('description');
-                    echo $this->Form->hidden('collection_id', ['value' => $collection->id]);
-                    echo $this->Form->control('source');
-                    /* echo $this->Form->control('metadata'); */
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
-    </div>
-</div>
+
+$this->extend('../Common/form');
+$this->assign('title', __('Add Element'));
+
+$this->start('form-creation');
+    echo $this->Form->create($element);
+$this->end();
+
+$this->start('form-fields');
+    echo $this->Form->control('name', [
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->control('description', [
+        'class' => 'form-control',
+    ]);
+    echo $this->Form->hidden('collection_id', ['value' => $collection->id]);
+    echo $this->Form->control('source', [
+        'class' => 'form-control',
+    ]);
+    /* echo $this->Form->control('metadata'); */
+$this->end();
+
+$this->start('form-end');
+    echo $this->Form->button(__('Submit'), [
+        'class' => 'btn btn-outline-danger',
+    ]);
+    echo $this->Form->end();
+    echo $this->Html->link(
+        __('Back'),
+        ['action' => 'view', $collection->id],
+        ['class' => 'btn btn-outline-danger']
+    );
+$this->end();
