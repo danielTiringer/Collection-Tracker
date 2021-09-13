1d4ba2a57679735be435e8687a335fa8a475d596
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-13 18:01:07 +0200

    Remove unused file

--- src/src/Policy/CollectionsTablePolicy.php
+++
@@ -1,9 +0,0 @@
-namespace App\Policy;
-
-class CollectionsTablePolicy
-{
-    public function scopeIndex($user, $query)
-    {
-        return $query->where(['Collections.users_id' => $user->getIdentifier()]);
-    }
-}
