b1b9440e56d3e0a09a11540c5305e65de0394e85
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-13 17:41:21 +0200

    Update the collectyionpolicy

--- src/src/Policy/CollectionPolicy.php
+++ src/src/Policy/CollectionPolicy.php
@@ -1,18 +1,58 @@
 <?php
+declare(strict_types=1);
+
 namespace App\Policy;
 
 use App\Model\Entity\Collection;
 use Authorization\IdentityInterface;
-use Authorization\Policy\Result;
 
 class CollectionPolicy
 {
-    public function canUpdate(IdentityInterface $user, Collection $collection)
+    /**
+     * Checks if the user can add a collection
+     *
+     * @param \Authorization\IdentityInterface $user the user in question
+     * @param \App\Model\Entity\Collection $collection the collection model
+     * @return bool
+     */
+    public function canAdd(IdentityInterface $user, Collection $collection): bool
     {
-        if ($user->id == $collection->users_id) {
-            return new Result(true);
-        }
+        return true;
+    }
 
-        return new Result(false, 'not-owner');
+    /**
+     * Checks if the user can add a collection
+     *
+     * @param \Authorization\IdentityInterface $user the user in question
+     * @param \App\Model\Entity\Collection $collection the collection model
+     * @return bool
+     */
+    public function canEdit(IdentityInterface $user, Collection $collection): bool
+    {
+        return $this->isAuthor($user, $collection);
+    }
+
+    /**
+     * Checks if the user can add a collection
+     *
+     * @param \Authorization\IdentityInterface $user the user in question
+     * @param \App\Model\Entity\Collection $collection the collection model
+     * @return bool
+     */
+    public function canDelete(IdentityInterface $user, Collection $collection): bool
+    {
+        return $this->isAuthor($user, $collection);
+    }
+
+    /**
+     * Checks if the user is the author of the collection
+     *
+     * @param \Authorization\IdentityInterface $user the user in question
+     * @param \App\Model\Entity\Collection $collection the collection model
+     * @return bool
+     */
+    protected function isAuthor(IdentityInterface $user, Collection $collection): bool
+    {
+        return $collection->users_id === $user->id;
     }
 }
