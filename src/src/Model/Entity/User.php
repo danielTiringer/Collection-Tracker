be20ac1b1a8feee410d2b913b35a476bf22a637a
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-15 07:52:42 +0200

    Add authorization to the user class

--- src/src/Model/Entity/User.php
+++ src/src/Model/Entity/User.php
@@ -5,6 +5,9 @@ namespace App\Model\Entity;
 
 use Authentication\IdentityInterface as AuthenticationIdentity;
 use Authentication\PasswordHasher\DefaultPasswordHasher;
+use Authorization\AuthorizationServiceInterface;
+use Authorization\IdentityInterface as AuthorizationIdentity;
+use Authorization\Policy\ResultInterface;
 use Cake\ORM\Entity;
 
 /**
@@ -17,8 +20,77 @@ use Cake\ORM\Entity;
  * @property \Cake\I18n\FrozenTime $created
  * @property \Cake\I18n\FrozenTime $modified
  */
-class User extends Entity implements AuthenticationIdentity
+class User extends Entity implements AuthenticationIdentity, AuthorizationIdentity
 {
+    /**
+     * Authorization Service
+     *
+     * @var \Authorization\AuthorizationServiceInterface
+     */
+    protected $authorization;
+
+    /**
+     * Check whether the current identity can perform an action.
+     *
+     * @param string $action The action/operation being performed.
+     * @param mixed $resource The resource being operated on.
+     * @return bool
+     */
+    public function can($action, $resource): bool
+    {
+        return $this->authorization->can($this, $action, $resource);
+    }
+
+    /**
+     * Check whether the current identity can perform an action.
+     *
+     * @param string $action The action/operation being performed.
+     * @param mixed $resource The resource being operated on.
+     * @return \Authorization\Policy\ResultInterface
+     */
+    public function canResult($action, $resource): ResultInterface
+    {
+        return $this->authorization->canResult($this, $action, $resource);
+    }
+
+    /**
+     * Apply authorization scope conditions/restrictions.
+     *
+     * @param string $action The action/operation being performed.
+     * @param mixed $resource The resource being operated on.
+     * @return mixed The modified resource.
+     */
+    public function applyScope($action, $resource)
+    {
+        return $this->authorization->applyScope($this, $action, $resource);
+    }
+
+    /**
+     * Get the decorated identity
+     *
+     * If the decorated identity implements `getOriginalData()`
+     * that method should be invoked to expose the original data.
+     *
+     * @return array|\ArrayAccess
+     */
+    public function getOriginalData()
+    {
+        return $this;
+    }
+
+    /**
+     * Setter to be used by the middleware.
+     *
+     * @param \Authorization\AuthorizationServiceInterface $service The authorization service interface
+     * @return array|\ArrayAccess
+     */
+    public function setAuthorization(AuthorizationServiceInterface $service)
+    {
+        $this->authorization = $service;
+
+        return $this;
+    }
+
     /**
      * Authentication\IdentityInterface method
      *
