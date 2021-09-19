3f6dbceb88a1242314c0f78974c271e7da50b651
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-19 17:45:08 +0200

    Add image field to the element

---
+++ src/config/Migrations/20210919154014_AddImageToElements.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class AddImageToElements extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('elements');
+        $table->addColumn('image', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => true,
+            'after' => 'source',
+        ]);
+        $table->update();
+    }
+}
