074ad86b3a2168d255cd333af928e63258ce8cd2
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-19 17:46:43 +0200

    Add image behavior to element

--- src/src/Model/Table/ElementsTable.php
+++ src/src/Model/Table/ElementsTable.php
@@ -3,9 +3,14 @@ declare(strict_types=1);
 
 namespace App\Model\Table;
 
+use ArrayObject;
+use Cake\Datasource\EntityInterface;
+use Cake\Event\Event;
+use Cake\I18n\Time;
 use Cake\ORM\RulesChecker;
 use Cake\ORM\Table;
 use Cake\Validation\Validator;
+use Josegonzalez\Upload\Validation\DefaultValidation;
 
 /**
  * Elements Model
@@ -48,6 +53,21 @@ class ElementsTable extends Table
             'foreignKey' => 'collection_id',
             'joinType' => 'INNER',
         ]);
+
+        $this->addBehavior('Josegonzalez/Upload.Upload', [
+            'image' => [
+                'path' => 'webroot{DS}img{DS}element-img{DS}',
+                'nameCallback' => function ($table, $entity, $data, $field, $settings) {
+                    $currentDateTime = (new Time('now'))->format('YmdHis');
+
+                    return $currentDateTime . '_' . $data->getClientFileName();
+                },
+                'deleteCallback' => function ($path, $entity, $field, $settings) {
+                    return [$path . $entity->{$field}];
+                },
+                'keepFilesOnDelete' => false,
+            ],
+        ]);
     }
 
     /**
@@ -79,6 +99,25 @@ class ElementsTable extends Table
             ->allowEmptyString('source');
 
         $validator
+            ->allowEmptyFile('image', null)
+            ->add('image', [
+                'mimeType' => [
+                    'rule' => ['mimeType', ['image/jpg', 'image/png', 'image/jpeg']],
+                    'message' => 'Only jpg, jpeg and png files can be uploaded.',
+                ],
+                'fileBelowMaxSize' => [
+                    'rule' => ['isBelowMaxSize', 10 * 1024 * 1024 * 1024],
+                    'message' => 'Image file size must be less than 10MB.',
+                    'provider' => 'upload',
+                ],
+                'fileCompletedUpload' => [
+                    'rule' => 'isCompletedUpload',
+                    'message' => 'This file could not be uploaded completely',
+                    'provider' => 'upload',
+                ],
+            ]);
+
+        $validator
             ->scalar('metadata')
             ->maxLength('metadata', 255)
             ->allowEmptyString('metadata');
@@ -98,5 +137,22 @@ class ElementsTable extends Table
         $rules->add($rules->existsIn(['collection_id'], 'Collections'), ['errorField' => 'collection_id']);
 
         return $rules;
+    }
+
+    /**
+     * Lifecycle callback after saving an entity
+     *
+     * @param \Cake\Event\Event $event The event
+     * @param \Cake\Datasource\EntityInterface $entity The entity
+     * @param \ArrayObject $options Custom options
+     * @return void
+     */
+    public function afterSave(Event $event, EntityInterface $entity, ArrayObject $options): void
+    {
+        $previousImage = WWW_ROOT . 'img' . DS . 'element-img' . DS . $options['previousImageName'];
+
+        if (is_file($previousImage)) {
+            unlink($previousImage);
+        }
     }
 }
