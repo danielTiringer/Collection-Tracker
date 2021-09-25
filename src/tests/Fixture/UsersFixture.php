6c4450a1b09c619558b8e89f7d3c55850a3bd82c
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-25 22:38:02 +0200

    Add more fixtures

--- src/tests/Fixture/UsersFixture.php
+++ src/tests/Fixture/UsersFixture.php
@@ -50,6 +50,14 @@ class UsersFixture extends TestFixture
                 'created' => '2021-05-02 10:39:39',
                 'modified' => '2021-05-02 10:39:39',
             ],
+            [
+                'id' => 2,
+                'name' => 'Other User',
+                'email' => 'other@example.com',
+                'password' => $hasher->hash('password'),
+                'created' => '2021-05-03 10:39:39',
+                'modified' => '2021-05-03 10:39:39',
+            ],
         ];
         parent::init();
     }
