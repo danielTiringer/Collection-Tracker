040833a37e5d253a6922fcbf66a86c9701d124d9
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-25 11:10:27 +0200

    Move fields to above methods

--- src/src/Model/Entity/User.php
+++ src/src/Model/Entity/User.php
@@ -23,11 +23,37 @@ use Cake\ORM\Entity;
 class User extends Entity implements AuthenticationIdentity, AuthorizationIdentity
 {
     /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array
+     */
+    protected $_accessible = [
+        'name' => true,
+        'email' => true,
+        'password' => true,
+        'created' => true,
+        'modified' => true,
+    ];
+
+    /**
+     * Fields that are excluded from JSON versions of the entity.
+     *
+     * @var array
+     */
+    protected $_hidden = [
+        'password',
+    ];
+
+    /**
      * Authorization Service
      *
-     * @var \Authorization\AuthorizationServiceInterface
+     * @var \Authorization\AuthorizationServiceInterface|null
      */
-    protected $authorization;
+    protected $authorization = null;
 
     /**
      * Check whether the current identity can perform an action.
@@ -100,32 +126,6 @@ class User extends Entity implements AuthenticationIdentity, AuthorizationIdenti
     {
         return $this->id;
     }
-
-    /**
-     * Fields that can be mass assigned using newEntity() or patchEntity().
-     *
-     * Note that when '*' is set to true, this allows all unspecified fields to
-     * be mass assigned. For security purposes, it is advised to set '*' to false
-     * (or remove it), and explicitly make individual fields accessible as needed.
-     *
-     * @var array
-     */
-    protected $_accessible = [
-        'name' => true,
-        'email' => true,
-        'password' => true,
-        'created' => true,
-        'modified' => true,
-    ];
-
-    /**
-     * Fields that are excluded from JSON versions of the entity.
-     *
-     * @var array
-     */
-    protected $_hidden = [
-        'password',
-    ];
 
     /**
      * Automatically hash passwords when they are changed.
