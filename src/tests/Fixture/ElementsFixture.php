b2e3b6b63f50e26f1d535de386601ecdc0d490f2
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-09-26 18:35:17 +0200

    Testing the view method

--- src/tests/Fixture/ElementsFixture.php
+++ src/tests/Fixture/ElementsFixture.php
@@ -56,6 +56,16 @@ class ElementsFixture extends TestFixture
                 'created' => '2021-06-03 20:31:26',
                 'modified' => '2021-06-03 20:31:26',
             ],
+            [
+                'id' => 2,
+                'name' => 'Lorem ipsum dolor sit amet',
+                'description' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'collection_id' => 3,
+                'source' => 'Lorem ipsum dolor sit amet',
+                'metadata' => 'Lorem ipsum dolor sit amet',
+                'created' => '2021-06-03 20:31:26',
+                'modified' => '2021-06-03 20:31:26',
+            ],
         ];
         parent::init();
     }
--- src/tests/TestCase/Controller/ElementsControllerTest.php
+++ src/tests/TestCase/Controller/ElementsControllerTest.php
@@ -3,6 +3,8 @@ declare(strict_types=1);
 
 namespace App\Test\TestCase\Controller;
 
+use App\Test\TestCase\LoginTrait;
+use Cake\ORM\TableRegistry;
 use Cake\TestSuite\IntegrationTestTrait;
 use Cake\TestSuite\TestCase;
 
@@ -13,6 +15,7 @@ use Cake\TestSuite\TestCase;
  */
 class ElementsControllerTest extends TestCase
 {
+    use LoginTrait;
     use IntegrationTestTrait;
 
     /**
@@ -23,16 +26,22 @@ class ElementsControllerTest extends TestCase
     protected $fixtures = [
         'app.Elements',
         'app.Collections',
+        'app.Users',
     ];
 
     /**
-     * Test index method
+     * Sets up the tests with common configuration
      *
      * @return void
      */
-    public function testIndex(): void
+    public function setUp(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->enableCsrfToken();
+
+        $this->enableRetainFlashMessages();
+
+        $this->Elements = TableRegistry::get('Elements');
+        $this->Users = TableRegistry::get('Users');
     }
 
     /**
@@ -40,9 +49,48 @@ class ElementsControllerTest extends TestCase
      *
      * @return void
      */
-    public function testView(): void
+    public function testViewSuccess(): void
     {
-        $this->markTestIncomplete('Not implemented yet.');
+        $this->login();
+
+        $this->get('/1/elements/1');
+
+        $this->assertResponseOk();
+        $this->assertSame(1, $this->viewVariable('element')->id);
+        $this->assertSame(1, $this->viewVariable('element')->collection_id);
+    }
+
+    /**
+     * Test view method unauthenticated
+     *
+     * @return void
+     */
+    public function testViewUnauthenticatedFails(): void
+    {
+        $this->get('/1/elements/1');
+
+        $this->assertResponseCode(302);
+        $this->assertRedirectEquals(['controller' => 'Users', 'action' => 'login']);
+        $this->assertTrue($this->Elements->exists(['id' => 1]));
+    }
+
+    /**
+     * Test view method unauthorized fails
+     *
+     * @return void
+     */
+    public function testViewUnauthorizedFails(): void
+    {
+        $this->login();
+
+        $this->get('/3/elements/2');
+
+        $this->assertResponseCode(403);
+        $this->assertTrue($this->Elements->exists(['id' => 2]));
+
+        $element = $this->Elements->find()->where(['id' => 2])->first();
+
+        $this->assertSame(3, $element->collection_id);
     }
 
     /**
