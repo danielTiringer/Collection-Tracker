cec74ef615043fd8a18d7397fcbc8cebe83457d7
Daniel Tiringer <tiringerdaniel@tutanota.com>
2022-02-06 16:05:32 +0100

    Change docker setup to alpine-nginx and db to postgres

--- src/config/app_local.example.php
+++ src/config/app_local.example.php
@@ -43,17 +43,17 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'database',
+            'driver' => 'Cake\Database\Driver\Postgres',
+            'host' => 'postgres',
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-
-            'username' => env('MYSQL_USER', 'my_app'),
-            'password' => env('MYSQL_PASSWORD', 'secret'),
-            'database' => env('MYSQL_DATABASE', 'my_app'),
+            'username' => env('POSTGRES_USER', 'my_app'),
+            'password' => env('POSTGRES_PASSWORD', 'secret'),
+            'database' => env('POSTGRES_DB', 'my_app'),
             /*
              * If not using the default 'public' schema with the PostgreSQL driver
              * set it here.
